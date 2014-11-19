0d2cc75e593d9f576548c511f36a547523962da9
Matt A. Tobin <email@mattatobin.com>
2014-11-19 08:21:19 -0500

    Globally replace GPL License Notice

--- chrome/content/ui/i18n.js
+++ chrome/content/ui/i18n.js
@@ -1,18 +1,28 @@
 /*
- * This file is part of Adblock Plus <http://adblockplus.org/>,
- * Copyright (C) 2006-2013 Eyeo GmbH
+ * This file is part of Adblock Latitude,
+ * Copyright (C) 2014 Binary Outcast.
  *
- * Adblock Plus is free software: you can redistribute it and/or modify
+ * The Original Code is AdBlock Plus
+ * The Initial Developer of the Original Code is
+ * Eyeo GmbH
+ *
+ * Portions created by the Initial Developer are 
+ * Copyright (C) 2006-2013 the Initial Developer. All Rights Reserved.
+ *
+ * Contributors:
+ *
+ *
+ * Adblock Latitude is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
  * published by the Free Software Foundation.
  *
- * Adblock Plus is distributed in the hope that it will be useful,
+ * Adblock Latitude is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Adblock Latitude.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 var i18n;
