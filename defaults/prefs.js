8577dbd6af5d517562467d0cc82f5b995c6b5778
Matt A. Tobin <email@mattatobin.com>
2014-11-21 07:14:39 -0500

    Port Allow users to enforce ABP being loaded completely at browser startup, even if delaying it (Part 2)

--- defaults/prefs.js
+++ defaults/prefs.js
@@ -32,3 +32,4 @@ pref("extensions.adblockplus.hideContributeButton", false);
 pref("extensions.adblockplus.blockableItemsSize", {width: 200, height: 200});
 pref("extensions.adblockplus.notificationurl", "");
 pref("extensions.adblockplus.notificationdata", {});
+pref("extensions.adblockplus.please_kill_startup_performance", false);
